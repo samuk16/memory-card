a34d5db8795457368fd7907d33345b7880572a65
Samuk <marcelolamas19@gmail.com>
2024-02-26 19:34:34 -0300

    Add routeHelpers with loader and action functions for react-router-dom

---
+++ src/utils/routerHelpers.ts
@@ -0,0 +1,28 @@
+import { redirect } from "react-router-dom";
+import {
+  setDifficult,
+  setSkinsState,
+  getDataForNames,
+  getAllDataChampions,
+} from "./fetchData";
+
+interface ActionData {
+  request: Request;
+}
+export async function action({ request }: ActionData) {
+  const formData = await request.formData();
+  setDifficult(
+    // parseInt(formData.get("difficult") ? formData.get("difficult") : "5"),
+    parseInt(formData.get("difficult")?.toString() ?? "5"),
+  );
+
+  const inputSkins = document.getElementById("sk");
+  setSkinsState((inputSkins as HTMLInputElement).checked);
+  return redirect(`/game`);
+}
+
+export async function loader() {
+  await getDataForNames();
+  const champions = getAllDataChampions();
+  return champions;
+}
