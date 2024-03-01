c16e858c11f50d653091f779cd05d53ae530f05d
Samuk <marcelolamas19@gmail.com>
2024-03-01 19:38:02 -0300

    Add props to disable the play variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,11 @@
 interface ButtonProps {
   children: React.ReactNode;
   variant: "play" | "confirm" | "square" | "cancel";
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset" | undefined;
+  form?: string;
+  playOff?: string;
 }
-
 const variants = {
   play: {
     "1div": "mr-auto relative flex justify-center items-center ml-20px",
@@ -11,37 +14,51 @@ const variants = {
     "3div":
       "borderGradietn flex justify-end items-center dinamic-bg-backgroundBtnBlue-svg bg-cover bg-no-repeat h-50px w-198px",
 
-    btn: "lol hover:text-[#cdfafa] relative w-163px h-42px bg-transparent border-none dinamic-bg-borderBtnBlue-svg bg-cover cursor-pointer mr-4px",
+    btn: "lol hover:text-[#cdfafa] transition-colors relative w-163px h-42px bg-transparent border-none dinamic-bg-borderBtnBlue-svg bg-cover cursor-pointer mr-4px ",
   },
   confirm: {
     "1div": "relative flex justify-center items-center",
     "2div": "",
     "3div":
       "borderGradietn flex justify-end items-center dinamic-bg-backgroundBtnRounded-svg bg-cover bg-no-repeat h-50px w-209px",
-    btn: "lol hover:text-[#cdfafa] relative w-163px h-42px bg-transparent border-none dinamic-bg-borderBtnBlue-svg bg-cover cursor-pointer mr-4px text-[#0AC8B9]",
+    btn: "lol hover:text-[#cdfafa] transition-colors relative w-163px h-42px bg-transparent border-none dinamic-bg-borderBtnBlue-svg bg-cover cursor-pointer mr-4px text-[#0AC8B9]",
   },
   square: {
-    "1div": "",
+    "1div":
+      "w-fit border-2 border-solid bg-origin-border border-transparent bg-clip-border bg-gradient-to-b from-[#C89B3C] to-[#785A28] cursor-pointer hover:brightness-130",
     "2div": "",
     "3div": "",
-    btn: "relative w-150px h-40px border-none dinamic-bg-bgBtnsqua-svg bg-contain bg-no-repeat cursor-pointer text-base",
+    btn: "w-150px h-40px text-base bg-black border-none cursor-pointer",
   },
 
   cancel: {
     "1div": "flex justify-center items-center",
     "2div": "",
     "3div": "",
-    btn: "btnT absolute top-1 left-1 w-42px h-42px border-none dinamic-bg-EllipseBtn-svg bg-contain bg-no-repeat cursor-pointer text-base bg-transparent flex justify-center items-center z-10",
+    btn: "btnText absolute top-1 left-1 transition-colors w-42px h-42px border-none dinamic-bg-EllipseBtn-svg bg-contain bg-no-repeat cursor-pointer text-base bg-transparent flex justify-center items-center z-10",
   },
 };
-function Button({ children, variant }: ButtonProps) {
+function Button({
+  children,
+  variant,
+  onClick,
+  type,
+  form,
+  playOff,
+}: ButtonProps) {
   return (
     <div className={variants[variant]["1div"]}>
       <div className={variants[variant]["2div"]}></div>
       <div className={variants[variant]["3div"]}>
-        <button className={variants[variant].btn}>
+        <button
+          className={variants[variant].btn + " " + playOff}
+          onClick={onClick}
+          type={type}
+          form={form}
+          disabled={playOff ? true : false}
+        >
           {children == "" ? (
-            <div className="x relative i-tabler-x text-[#C8AA6E] w-28px h-28px hover:text-[#F0E6D2]"></div>
+            <div className="x relative i-tabler-x text-[#C8AA6E] w-28px h-28px hover:text-[#F0E6D2] transition-colors"></div>
           ) : (
             children
           )}
