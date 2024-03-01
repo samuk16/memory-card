4973f0a03e0786bc4af31b01989c2ed65f7dec44
Samuk <marcelolamas19@gmail.com>
2024-03-01 19:45:02 -0300

    Add reac-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,40 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import 'virtual:uno.css';
+import "./App.css";
+import "virtual:uno.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { StartScreen } from "./components/StartScreen.tsx";
+import { DifficultyMenu } from "./components/DifficultyMenu.tsx";
+import { action as difficultyAction } from "./utils/routerHelpers.ts";
+import { loader as gameLoader } from "./utils/routerHelpers.ts";
+import { GameBoard } from "./components/GameBoard.tsx";
+import { Root } from "./components/Root.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <StartScreen />,
+      },
+      {
+        path: "difficult",
+        element: <DifficultyMenu />,
+        action: difficultyAction,
+      },
+      {
+        path: "game",
+        element: <GameBoard />,
+        loader: gameLoader,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />,
 );
