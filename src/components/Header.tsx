194a92802254d75834724232d7b16e6da1bb968c
Samuk <marcelolamas19@gmail.com>
2024-03-01 19:51:56 -0300

    Disable play button when game is in progress

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
 import { Button } from "./Button";
+import { Link } from "react-router-dom";
 
-function Header() {
+function Header({ locationPath }: { locationPath: string }) {
+  function checkLocationPath() {
+    return locationPath !== "/" ? "grayscale brightness-50" : "";
+  }
   return (
-    <header className=" relative grid grid-cols-3 p-10px border-b-1 border-style-solid border-transparent border-b-[#C8AA6E]">
-      <Button children="PLAY" variant="play" />
-      <h1 className="justify-self-center">Memory Legends</h1>
+    <header className=" absolute top-0 w-screen grid grid-cols-3 items-center p-10px border-b-1 border-style-solid border-transparent border-b-[#C8AA6E] z-12">
+      <Link to={"/difficult"} className="w-fit no-underline">
+        <Button children="PLAY" variant="play" playOff={checkLocationPath()} />
+      </Link>
+      <h1 className="justify-self-end xl:text-5xl md:text-3xl text-2xl sm:col-start-2 col-start-3 sm:justify-self-center">
+        Memory Legends
+      </h1>
     </header>
   );
 }
