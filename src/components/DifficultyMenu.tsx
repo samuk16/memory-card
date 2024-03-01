a807b0e622ed7105a11489a66b38dae86d3d89b5
Samuk <marcelolamas19@gmail.com>
2024-03-01 20:16:05 -0300

    Integrate Form and Link components in difficulty selection screen

--- src/components/DifficultyMenu.tsx
+++ src/components/DifficultyMenu.tsx
@@ -1,14 +1,11 @@
 import { Button } from "./Button.tsx";
-import { Header } from "./Header.tsx";
-import { setDifficult, setSkinsState } from "../utils/fetchData.ts";
-import { Form, redirect, useNavigate } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
+
 function DifficultyMenu() {
-  const navigate = useNavigate();
   return (
-    <div className="flex flex-col justify-between w-screen h-screen dinamic-bg-bgDifficultMenu-webp bg-no-repeat bg-cover">
-      <div className="absolute w-full h-full opacity-60 bg-gradient-to-b from-[#0A1428] to-[#091428]"></div>
-      <Header />
-      <div className="flex items-center justify-center flex-col gap-10 relative h-full">
+    <div className="flex flex-col justify-between w-screen h-[100dvh] dinamic-bg-bgDifficultMenu-webp bg-no-repeat bg-cover transition-all">
+      <div className="absolute w-full h-[100dvh] opacity-60 bg-gradient-to-b from-[#0A1428] to-[#091428] z-1 transition-all"></div>
+      <div className="flex items-center justify-center flex-col gap-10 relative h-[100dvh] z-2 transition-all">
         <h2 className="text-[#C8AA6E] text-3xl">CHOOSE DIFFICULTY</h2>
 
         <Form
@@ -16,11 +13,9 @@ function DifficultyMenu() {
           method="post"
           className="flex gap-10 flex-col items-center"
         >
-          {/* <div></div> */}
-          {/* <div className="flex gap-10 flex-col h-full"> */}
           <label
             htmlFor="ez"
-            className="lab1 relative text-[#A09B8C] flex gap-2 w-100px"
+            className="lab1 relative text-[#A09B8C] flex gap-2 w-100px hover:text-[#F0E6D2] transition-colors ease-in-out has-[:checked]:text-[#F0E6D2]"
           >
             <input
               type="radio"
@@ -34,7 +29,7 @@ function DifficultyMenu() {
           </label>
           <label
             htmlFor="md"
-            className="lab1 text-[#A09B8C] flex gap-2 w-100px"
+            className="lab1 text-[#A09B8C] flex gap-2 w-100px hover:text-[#F0E6D2] transition-colors ease-in-out has-[:checked]:text-[#F0E6D2]"
           >
             <input
               type="radio"
@@ -48,7 +43,7 @@ function DifficultyMenu() {
           </label>
           <label
             htmlFor="hr"
-            className="lab1 text-[#A09B8C] flex gap-2 w-100px"
+            className="lab1 text-[#A09B8C] flex gap-2 w-100px hover:text-[#F0E6D2] transition-colors ease-in-out has-[:checked]:text-[#F0E6D2]"
           >
             <input
               type="radio"
@@ -60,8 +55,6 @@ function DifficultyMenu() {
             HARD
             <span className=" sp1 absolute top-0 left-0 h-20px w-20px"></span>
           </label>
-
-          {/* </div> */}
         </Form>
         <div className="w-300px h-2px bg-[#C8AA6E]"></div>
 
@@ -72,7 +65,7 @@ function DifficultyMenu() {
         >
           <label
             htmlFor="sk"
-            className="lab1 gr text-[#A09B8C] flex gap-2 w-100px"
+            className="lab1 gr text-[#A09B8C] flex gap-2 w-100px hover:text-[#F0E6D2] transition-colors ease-in-out has-[:checked]:text-[#F0E6D2]"
           >
             <input
               type="checkbox"
@@ -86,43 +79,20 @@ function DifficultyMenu() {
           </label>
         </Form>
       </div>
-      <div className="relative self-center">
-        {/* <Link to={"/"} className="w-fit no-underline"> */}
-        <Button variant="cancel" children="" onClick={() => navigate(-1)} />
-        {/* </Link> */}
-        {/* <Link to={"/game"} className="w-fit no-underline"> */}
+      <div className="relative self-center bottom-15 z-2">
+        <Link to={"/"} className="w-fit no-underline">
+          <Button variant="cancel" children="" />
+        </Link>
+
         <Button
           variant="confirm"
           children="CONFIRM"
           type="submit"
           form="dificultyForm"
+          onClick={() => {}}
         ></Button>
-        {/* </Link> */}
       </div>
-
-      <footer className="self-center p-10px text-xs font-spiegel">
-        Memory Legends is not endorsed by Riot Games and does not reflect the
-        views or opinions of Riot Games or anyone officially involved in
-        producing or managing Riot Games properties.
-        <br />
-        Riot Games and all associated properties are trademarks or registered
-        trademarks of Riot Games, Inc
-      </footer>
     </div>
   );
 }
-interface ActionData {
-  request: Request;
-}
-export async function action({ request }: ActionData) {
-  const formData = await request.formData();
-  setDifficult(
-    parseInt(formData.get("difficult") ? formData.get("difficult") : "5"),
-  );
-
-  const inputSkins = document.getElementById("sk");
-  setSkinsState((inputSkins as HTMLInputElement).checked);
-  return redirect(`/game`);
-}
-
 export { DifficultyMenu };
