5715c261e542419590756067080f0cebcc851705
Samuk <marcelolamas19@gmail.com>
2024-03-01 19:40:13 -0300

    Improve responsive

--- src/components/PageLoading.tsx
+++ src/components/PageLoading.tsx
@@ -9,8 +9,7 @@ function PageLoading({ visible, variant }: PageLoadingProps) {
     loading1: {
       l1: `w-screen h-screen absolute flex items-center justify-center bg-[#040d16] z-11 transition-opacity duration-150 
       ${visible ? "opacity-100" : "opacity-0"}`,
-      l2: "w-screen h-screen absolute flex items-center justify-center bg-[#040d16] z-11",
-      // btn: "lol hover:text-[#cdfafa] relative w-163px h-42px bg-transparent border-none dinamic-bg-borderBtnBlue-svg bg-cover cursor-pointer mr-4px ",
+      l2: "w-screen sm:absolute sm:h-screen fixed top-0 bottom-0 flex items-center justify-center bg-[#040d16] z-11",
     },
   };
   return (
