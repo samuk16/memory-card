f398e44d91c5796a393e460645cc8d5368c15500
Samuk <marcelolamas19@gmail.com>
2024-03-01 21:09:06 -0300

    Enhance uno.config.ts with rules, theme customizations, and preflights

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,41 @@
-import {defineConfig} from 'unocss'
-import { presetUno } from 'unocss'
+import { defineConfig } from "unocss";
+import { presetUno } from "unocss";
+import { presetIcons } from "unocss";
+
 export default defineConfig({
-    presets: [
-    presetUno(),
+  presets: [presetUno(), presetIcons()],
+  rules: [
+    [
+      /^dinamic-bg-(.*)$/,
+      ([, d]) => {
+        const img = d.split("-")[0];
+        const type = d.split("-")[1];
+        return {
+          "background-image": `url('/src/assets/${img}.${type}')`,
+        };
+      },
+    ],
   ],
-})
+  theme: {
+    fontFamily: {
+      beaufort: "beaufort",
+      spiegel: "spiegel",
+    },
+  },
+  preflights: [
+    {
+      getCSS() {
+        return `
+      @font-face {
+        font-family: beaufort;
+        src: url(src/assets/fonts/BeaufortforLOL-Bold.ttf);
+      }
+      @font-face {
+        font-family: spiegel;
+        src: url(src/assets/fonts/Spiegel_TT_Regular.ttf);
+      }
+     `;
+      },
+    },
+  ],
+});
