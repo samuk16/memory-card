6e46b0cecc14c5615c23a7e897119ddcacb6e51e
Samuk <marcelolamas19@gmail.com>
2024-03-06 13:07:57 -0300

    Integrated interfaces to type-annotate component variables

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,13 +1,16 @@
-let champions: any;
+import { Champions } from "./championInterfaces";
+import { Champion } from "./championDataInterfaces";
+let champions: Champions["data"];
 const arrChampiosns: string[] = [];
-interface Champion {
+interface Champion1 {
   name: string;
   skinId: number;
 }
+
 const championsArr: string[] = [];
 let difficult: number | null = 0;
 let skins: boolean;
-let arrData: any[];
+let arrData: Champion[];
 let listChecked: string[] = [];
 async function fetchData(): Promise<void> {
   try {
@@ -16,7 +19,7 @@ async function fetchData(): Promise<void> {
       { mode: "cors" },
     );
 
-    const championsData = await response.json();
+    const championsData: Champions = await response.json();
 
     champions = championsData.data;
     extractCh();
@@ -25,7 +28,7 @@ async function fetchData(): Promise<void> {
   }
 }
 
-async function getDataChampion(name: string): Promise<void> {
+async function getDataChampion(name: string): Promise<Champion> {
   try {
     const response = await fetch(
       `https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion/${name}.json`,
@@ -35,6 +38,7 @@ async function getDataChampion(name: string): Promise<void> {
     return championData.data;
   } catch (error) {
     console.log(error);
+    throw error;
   }
 }
 
@@ -42,18 +46,24 @@ async function getDataForNames(): Promise<void> {
   try {
     await fetchData();
     getChampions(getDifficult());
-    // listChecked = [];
     const promises = listChecked.map(getDataChampion);
-
-    const results = await Promise.all(promises);
-    arrData = results;
+    // const results = await Promise.all(promises);
+    const results = await Promise.all(
+      promises.map((p) =>
+        p.catch((e) => {
+          console.log(e);
+          return undefined;
+        }),
+      ),
+    );
+    arrData = results.filter((result) => result !== undefined) as Champion[];
   } catch (error) {
     console.log(error);
   }
 }
-function getRandomSkinId(objs: any, arrNames: string[]) {
+function getRandomSkinId(objs: Champion[], arrNames: string[]) {
   let objData;
-  const arrData: Champion[] = [];
+  const arrData: Champion1[] = [];
   for (let i = 0; i < arrNames.length; i++) {
     objData = {
       name: objs[i][arrNames[i]].id,
@@ -98,9 +108,10 @@ function getRandomChampions() {
 }
 
 function extractCh() {
+  // console.log(champions);
   for (const key in champions) {
     if (Object.prototype.hasOwnProperty.call(champions, key)) {
-      const childObj = champions[key];
+      const childObj = champions[key as keyof Champions];
       if (Object.prototype.hasOwnProperty.call(childObj, "name")) {
         arrChampiosns.push(childObj.id);
       }
@@ -124,11 +135,12 @@ function getSkinsState() {
   return skins;
 }
 
-function shuffleArray(array) {
+function shuffleArray(array: Champion1[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
+  // console.log(array);
   return array;
 }
 
