57f1fedf054942f3025508fd8cf19fd1a82e71fa
Samuk <marcelolamas19@gmail.com>
2024-03-06 16:11:29 -0300

    Change the color of the text

--- src/components/EndGame.tsx
+++ src/components/EndGame.tsx
@@ -10,10 +10,10 @@ function EndGame({ finalGrade, title }: EndGameProps) {
       <div className="sm:absolute fixed inset-0 w-full h-full z-12 backdrop-blur-sm transition-all"></div>
       <div className="sm:absolute fixed inset-0 w-full h-full  opacity-50 bg-gradient-to-b from-[rgba(0,0,0,0.7)] to-[#000000] z-12 transition-all"></div>
       <div className="sm:absolute fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-502px h-249px dinamic-bg-borderEndGame1-svg bg-transparent border-none flex flex-col items-center justify-between pb-3px pl-2px pt-20px z-13 drop-shadow-[0_0px_0px_rgba(183,157,103,.1)] hover:drop-shadow-[0_0px_15px_rgba(183,157,103,.5)] transition-all">
-        <p className="text-2xl">{title}</p>
+        <p className="text-2xl text-[#ffffffde]">{title}</p>
 
         <div className="w-120px h-140px dinamic-bg-bgPoint3-svg bg-transparent border-none bg-no-repeat bg-cover z-10 flex items-center justify-center pb-20px pl-5px">
-          <p className="text-4xl">{finalGrade}</p>
+          <p className="text-4xl text-[#ffffffde]">{finalGrade}</p>
         </div>
 
         <Link to={"/difficult"} className="w-fit no-underline">
