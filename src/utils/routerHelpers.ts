bab8813f92a2c473358a56e9e704aa433f91b0f7
Samuk <marcelolamas19@gmail.com>
2024-03-06 15:21:12 -0300

    Remove comments

--- src/utils/routerHelpers.ts
+++ src/utils/routerHelpers.ts
@@ -5,22 +5,17 @@ import {
   getDataForNames,
   getAllDataChampions,
 } from "./fetchData";
-
 interface ActionData {
   request: Request;
 }
 export async function action({ request }: ActionData) {
   const formData = await request.formData();
-  setDifficult(
-    // parseInt(formData.get("difficult") ? formData.get("difficult") : "5"),
-    parseInt(formData.get("difficult")?.toString() ?? "5"),
-  );
+  setDifficult(parseInt(formData.get("difficult")?.toString() ?? "5"));
 
   const inputSkins = document.getElementById("sk");
   setSkinsState((inputSkins as HTMLInputElement).checked);
   return redirect(`/game`);
 }
-
 export async function loader() {
   await getDataForNames();
   const champions = getAllDataChampions();
